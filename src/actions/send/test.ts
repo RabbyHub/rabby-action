a29206060f1101e920b63775f337e95d8e1eca36
qingwei-li <heisenberg-2077@outlook.com>
2024-08-26 14:19:20 +0800

    test: send typedData

--- src/actions/send/test.ts
+++ src/actions/send/test.ts
@@ -2,19 +2,27 @@ import {
   apiProvider,
   ETH_CHAIN_ID,
   formatProvider,
+  ORIGIN,
   SENDER,
   walletProvider,
 } from '../../../__mocks__';
 import { ParsedTransactionActionData } from '../../types';
 import { fetchDataSend } from './fetchData';
 import { formatSecurityEngineSend } from './formatSecurityEngine';
-import { parseTxData, preExecData, txData } from './mocks';
+import {
+  parseTxData,
+  parseTxDataTypedData,
+  preExecData,
+  txData,
+  txDataTypedData,
+} from './mocks';
 import { parseActionSend } from './parseAction';
 import {
   parseAction,
   fetchActionRequiredData,
   formatSecurityEngineContext,
 } from '../..';
+import { parseTypedDataAction } from '../../utils/parseTypedDataAction';
 
 /**
  * https://metamask.github.io/test-dapp/#sendButton
@@ -24,7 +32,7 @@ import {
 test.each([
   [parseActionSend, fetchDataSend, formatSecurityEngineSend],
   [parseAction, fetchActionRequiredData, formatSecurityEngineContext],
-])('Send', async (_parseAction, _fetchData, _format) => {
+])('Send -> Transaction', async (_parseAction, _fetchData, _format) => {
   const actionData = _parseAction({
     type: 'transaction',
     data: parseTxData['action'],
@@ -55,6 +63,49 @@ test.each([
     chainId: ETH_CHAIN_ID,
     isTestnet: false,
     provider: formatProvider,
+  });
+  expect(ctx).toMatchSnapshot('formatSecurityEngineSend');
+});
+
+/**
+ * https://syncswap.xyz/
+ * gas use USDT
+ * ETH -> USDC.e
+ */
+test.skip.each([
+  [
+    parseTypedDataAction(parseActionSend),
+    fetchDataSend,
+    formatSecurityEngineSend,
+  ],
+  [parseAction, fetchActionRequiredData, formatSecurityEngineContext],
+])('Send -> TypedData', async (_parseAction, _fetchData, _format) => {
+  const actionData = _parseAction({
+    type: 'typed_data',
+    data: parseTxDataTypedData['action'],
+    typedData: txDataTypedData,
+    sender: SENDER,
+  });
+  expect(actionData).toMatchSnapshot('parseActionSend');
+
+  const requireData = await _fetchData({
+    type: 'typed_data',
+    actionData,
+    chainId: ETH_CHAIN_ID,
+    walletProvider,
+    apiProvider,
+    sender: SENDER,
+  });
+  expect(requireData).toMatchSnapshot('fetchDataSend');
+
+  const ctx = await _format({
+    type: 'typed_data',
+    actionData,
+    requireData,
+    chainId: ETH_CHAIN_ID,
+    isTestnet: false,
+    provider: formatProvider,
+    origin: ORIGIN,
   });
   expect(ctx).toMatchSnapshot('formatSecurityEngineSend');
 });
