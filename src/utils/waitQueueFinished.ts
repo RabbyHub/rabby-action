547d7c66cb7293dd76e85fcf74ff432106a8998e
Hodor <lost1q84@gmail.com>
2025-04-02 10:56:58 +0800

    fix: waitQueueFinished

--- src/utils/waitQueueFinished.ts
+++ src/utils/waitQueueFinished.ts
@@ -2,6 +2,9 @@ import PQueue from 'p-queue';
 
 export const waitQueueFinished = (q: PQueue) => {
   return new Promise((resolve) => {
+    if (q.pending <= 0) {
+      resolve(null);
+    }
     q.on('empty', () => {
       if (q.pending <= 0) resolve(null);
     });
